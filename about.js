9357be59831f2884c923f3549d7fbd4c76f5d17f
Brevin Baskaran <brevin.baskaran@outlook.com>
2021-10-25 10:37:43 -0400

    Added nav bar to about us page

---
+++ about.js
@@ -0,0 +1,26 @@
+function Brevin() {
+    window.open(
+      "https://www.linkedin.com/in/brevin-baskaran-4b060b215", "_blank");
+}
+
+function Rasheek() {
+    window.open(
+      "https://www.linkedin.com/in/brevin-baskaran-4b060b215", "_blank");
+}
+
+function Vince() {
+    window.open(
+      "https://www.linkedin.com/in/brevin-baskaran-4b060b215", "_blank");
+}
+
+function Melina() {
+    window.open(
+      "https://www.linkedin.com/in/brevin-baskaran-4b060b215", "_blank");
+}
+
+function Reyaan() {
+    window.open(
+      "https://www.linkedin.com/in/brevin-baskaran-4b060b215", "_blank");
+}
+
+
